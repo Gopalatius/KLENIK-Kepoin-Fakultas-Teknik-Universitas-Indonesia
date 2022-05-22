97d679361cc9e450153eeb7fb0ac6515e2d34aee
Gopalatius <muhammad.naufal08@ui.ac.id>
2022-05-22 23:05:15 +0700

    the real client is here

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// wkwkwk
+//import packages
+const app = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+
+//initialize the app as an express app
+const app = express();
+const router = express.Router();
+const { Client } = require("pg");
+const bcrypt = require("bcrypt");
+
+//Insiasi koneksi ke database
+const db = new Client({
+    host: "naufal-sbd.postgres.database.azure.com",
+    port: 5432,
+    database: "naufal_9",
+    user: "naufal_sbd",
+    password:
+      "r0ZGRJllVnc%V3plCHD9tS0rva!VXTsWJvL4OsUIVLFpCpHKtMp&gubpbwa!gZKbyU*cKoPz24Gh*Ci@fmqa#9pqKP2MMcHtX^YSE8Q5aiVNRgoPWQE*vKFiFhO3%9tp",
+    ssl: true,
+  });
+  
