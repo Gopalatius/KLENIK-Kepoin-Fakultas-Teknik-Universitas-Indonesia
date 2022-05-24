dcbc6fcec5b6c88c8115e3bd80ab7e707ac2671a
Gopalatius <muhammad.naufal08@ui.ac.id>
2022-05-24 21:14:27 +0700

    merapikan js like a pro menggunakan guard clauses

--- index.js
+++ index.js
@@ -75,62 +75,34 @@ app.use(
 
 //Router 1: Menampilkan landing page (login/register)
 router.get("/", (req, res) => {
-    if (req.session.authenticated) {
-        //jika user terdaftar maka akan masuk ke halaman menu
-        return res.redirect("/menu");
-    } else {
-        fs.readFile("./login.html", null, function (error, data) {
-            if (error) {
-                res.writeHead(404);
-                alert("File tidak ditemukan!");
-            } else {
-                const minified = minify(data, minify_options);
-                return res.end(minified);
-            }
-        });
-    }
+    if (req.session.authenticated) return res.redirect("/menu")
+    fs.readFile("./login.html", null, function (error, data) {
+        if (error) return res.status(404).end('fail');
+        return res.end(minify(data, minify_options));
+    });
 });
 router.post("/login", (req, res) => {
-    const query = `SELECT * FROM user_reg WHERE username ='${req.body.username}';`;
-
+    const query = `SELECT * FROM user_reg WHERE username ='${req.body.username}';`
     db.query(query, (err, results) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        //
-        if (results.rowCount === 0) {
-            return res.status(400).end("No Username");
-        } else {
-            if (bcrypt.compare(req.body.password, results.rows[0]["password"])) {
-                req.session.authenticated = true;
-                req.session.user_id = results.rows[0]["user_id"];
-                req.session.username = results.rows[0]["username"];
-                return res.status(200).end("done");
-            }
+        if (err) return console.log(err)
+        if (results.rowCount === 0) return res.status(400).end("No Username")
+        if (bcrypt.compare(req.body.password, results.rows[0]["password"])) {
+            req.session.authenticated = true;
+            req.session.user_id = results.rows[0]["user_id"];
+            req.session.username = results.rows[0]["username"];
+            return res.status(200).end("done");
         }
     });
 });
 router.get("/register", (req, res) => {
-    temp = req.session;
-
-    if (req.session.authenticated) {
-        //jika user terdaftar maka akan masuk ke halaman menu
-        return res.redirect("/menu");
-    } else {
-        fs.readFile("./register.html", null, function (error, data) {
-            if (error) {
-                res.writeHead(404);
-                alert("File tidak ditemukan!");
-            } else {
-                const minified = minify(data,minify_options)
-                return res.end(minified);
-            }
-        });
-    }
+    if (req.session.authenticated) return res.redirect("/menu");
+    fs.readFile("./register.html", null, function (error, data) {
+        if (error) return res.status(404).end('fail');
+        return res.end(minify(data, minify_options));
+    });
+    
 });
 router.post("/register", (req, res) => {
-    temp = req.session;
     temp.username = req.body.username;
     temp.password = req.body.password;
     temp.role = req.body.role;
@@ -140,11 +112,7 @@ router.post("/register", (req, res) => {
      ('${temp.username}','${hashed_password}','${temp.role}',now());`;
 
     db.query(query, (err, results) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log("Berhasil memasukkan username dan password");
+        if (err) return console.log(err)
         res.write(`<html>
       <head>
           <title>Berhasil registrasi</title>
@@ -156,7 +124,7 @@ router.post("/register", (req, res) => {
       `);
     });
 
-    res.end("done");
+    return res.end("done");
 });
 router.post("/logout", (req, res) => {
     req.session.destroy();
@@ -174,19 +142,14 @@ router.post("/logout", (req, res) => {
 });
 router.get("/menu", (req, res) => {
     let file_html;
-    if (!req.session.authenticated) {
+    if (!req.session.authenticated) 
         file_html = "./illegal_access.html";
-    } else {
+    else {
         file_html = "./menu.html";
     }
     fs.readFile(file_html, null, function (error, data) {
-        if (error) {
-            res.writeHead(404);
-            alert("File tidak ditemukan!");
-        } else {
-            const minified = minify(data,minify_options)
-            return res.end(minified);
-        }
+        if (error) return res.status(404).end('fail');
+        return res.end(minify(data, minify_options));
     });
 });
 //--------------------Kawasan Teritori Azhari muehehehhe ----------------------------------------------------------
